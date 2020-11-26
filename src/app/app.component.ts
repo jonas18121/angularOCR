6487e9033d6f7592968454eda95dbabc7f130fa4
jonas18121 <jonasxtrm971@gmail.com>
2020-11-26 18:06:17 +0100

    Les Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 // les import doivent être toujours ecrit en 1er, afin d'évité des erreurs
 
 // component de base
@@ -9,5 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit
 {
-    ngOnInit(){}
+
+    secondes: number;
+
+    ngOnInit(){
+        const counter = Observable.interval(1000);
+
+        counter.subscribe(
+            (value) => {
+                this.secondes = value;
+            },
+
+            (error) => {
+                console.log('ERREUR : ' + error);
+                
+            },
+
+            () => {
+                console.log('Observable complete !');
+            }
+        );
+    }
 }
