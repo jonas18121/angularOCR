87e06e0c593e604c47b9e291d84a0bdbbfc1485f
jonas18121 <jonasxtrm971@gmail.com>
2020-11-28 15:01:46 +0100

    Écoutez l'utilisateur avec les Forms - méthode template , creer le formulaire

---
+++ src/app/add-appareil/add-appareil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-appareil',
+  templateUrl: './add-appareil.component.html',
+  styleUrls: ['./add-appareil.component.scss']
+})
+export class AddAppareilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
